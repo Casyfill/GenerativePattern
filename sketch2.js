00896f69ca9d3e8c2295c71441fd347a8d940ab8
casyfill <casyfill@gmail.com>
2015-06-15 20:54:41 +0300

    first try: slider and a canvas

--- sketch2.js
+++ sketch2.js
@@ -1,11 +1,32 @@
 
 
 function setup() {
-  createCanvas(480, 120);
-  stroke(0, 102);
 
-  s = createSlider(0,10,5);
-  s.size(120);
+  gui = getElement("gui")
+  canvas = getElement("canvas")
 
+  // скуф
+  c = createCanvas(1040, 300);
+  c.parent(canvas)
+  background(150, 150, 150);
+  
+  var dflt = 100;
+  sl1 = createSlider(0,255,dflt);
+  sl1.size('100px');
+  sl1.parent(gui)
+  
+
+  t1 = createP(dflt);
+  t1.parent(gui)
+  t1.id('t')
+
+
+}
+
+function draw() {
+
+	var n1 = sl1.value()
+	t1.html(n1)
+	background(n1);
 }
 
